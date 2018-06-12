e36d8a58541131e0ab1bab275e923c69630807a6
김예지 <soph456@naver.com>
2018-06-12 16:16:41 +0900

    add struct Label, Error Message, enum TableCommand and Type

--- ucode.h
+++ ucode.h
@@ -1,4 +1,3 @@
-
 #ifndef __UCODE_HEADER
 #define __UCODE_HEADER
 
@@ -6,6 +5,8 @@
 
 #define LABEL_SIZE           8
 #define SYMTAB_ARRAY_SIZE    512
+#define LABEL_MAX 256
+
 
 typedef enum {
     SPEC_NONE, SPEC_VOID, SPEC_INT
@@ -15,26 +16,50 @@ typedef enum {
     QUAL_NONE, QUAL_FUNC, QUAL_PARA, QUAL_CONST, QUAL_VAR
 } Qualifier;
 
+typedef enum{
+    PUSH=-100, POP
+} TableCommand;
+
+typedef enum{
+    BACK_CONDITION=-200, ESCAPE
+} Type;
+
 typedef struct _SymbolRow {
-    char *id;
-    Specifier spec;
+    char *id; // token value
+    Specifier spec; //void, int, none
     Qualifier qual;
-    int offset;
-    int width;
-    int base;
-    int init;
+    int offset; //function name의 offset은 0, const도 0, table->offset의 초기값은 1인데, 배열 넣을 때 아마 offset 쓰는 것 같음
+    int width;  //변수의 크기 or 함수의 parameter의 개수
+    int base;   //block number
+    int init;   //초기 값
     struct _SymbolTable *table;
 } SymbolRow;
 
-typedef struct _SymbolTable
-{
-    char *name;
-    int count;      
+typedef struct _SymbolTable {
+    char *name; //function name
+    int count;  //모든 지역변수의 총 량
     int offset;
-    int base;
+    int base; //lexical block
     SymbolRow rows[SYMTAB_ARRAY_SIZE];
+
 } SymbolTable;
 
+typedef struct _Label{
+    char label[LABEL_SIZE];
+    int type; //{ BACK_CONDITION, ESCAPE }
+} Label;
+
+char* message[9]={
+    "0: Not yet implemented"
+    ,"1: LabelTable is fulled"
+    ,"2: Dosen't have FUNC_HEAD"
+    ,"3: Dosen't have FUNC_DEF or DCL"
+    ,"4: Not DCL_SPEC"
+    ,"5: Not DCL_ITEM"
+    ,"6: dosen't have SIMPLE_VAR or ARRAY_VAR"
+    ,"7: Invalid function return type"
+    ,"8: Doesn't have PARAM_DCL"
+};
 
 void codeGen(Node *ptr, FILE *ucoFile);
 
